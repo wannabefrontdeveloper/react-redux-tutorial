74fd371b1b2a6814312ae4c87c41fea480153565
wannabefrontdeveloper <giwonk@naver.com>
2024-01-04 09:59:31 +0900

    redux-devtoos-extension을 적용하였습니다.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
