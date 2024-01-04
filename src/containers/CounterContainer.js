c95b8e6cf14d1c10690c8ed7557961b75a5f0141
wannabefrontdeveloper <giwonk@naver.com>
2024-01-04 13:34:04 +0900

    코드수정 useCallback으로 액션을 디스패치하는 함수를 감싸주었습니다.

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -5,12 +6,10 @@ import { increase, decrease } from '../modules/counter';
 const CounterContainer = () => {
   const number = useSelector((state) => state.counter.number);
   const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter
-      number={number}
-      onIncrease={() => dispatch(increase())}
-      onDecrease={() => dispatch(decrease())}
-    />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
 
