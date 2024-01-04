9298fe8e7b9558bb9af0d96bfe6a67fc340655f3
wannabefrontdeveloper <giwonk@naver.com>
2024-01-04 10:19:52 +0900

    connect 함수 내부에 익명 함수 형태로 선언하였습니다.

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -8,17 +8,12 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-
-const mapDisPatchToProps = (dispatch) => ({
-  //임시 함수
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-export default connect(mapStateToProps, mapDisPatchToProps)(CounterContainer);
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  }),
+)(CounterContainer);
