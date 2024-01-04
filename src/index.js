209df3e53d45af285309a85c761b209ed6281ee1
wannabefrontdeveloper <giwonk@naver.com>
2024-01-04 09:53:48 +0900

    스토어를 사용할 수 있도록 수정하였습니다.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
