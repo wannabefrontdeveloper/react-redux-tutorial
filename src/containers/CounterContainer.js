06d3fefdb96b2dd642d3612fa54449d08b045368
wannabefrontdeveloper <giwonk@naver.com>
2024-01-04 10:06:03 +0900

    CounterContainer에서 connect를 사용하도록하였습니다.

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,7 +1,23 @@
+import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 
-const CounterContainer = () => {
-  return <Counter />;
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter nubmer={number} onIncrease={increase} onDecrease={decrease} />
+  );
 };
 
-export default CounterContainer;
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+});
+
+const mapDisPatchToProps = (dispatch) => ({
+  //임시 함수
+  increase: () => {
+    console.log('increase');
+  },
+  decrease: () => {
+    console.log('decrease');
+  },
+});
+export default connect(mapStateToProps, mapDisPatchToProps)(CounterContainer);
